fea5f061302d5c748cb308cdd2d25d768fb9658e
dlwndud30 <leejy3022@naver.com>
2022-04-22 00:54:53 +0900

    계약서 추가 - html파일 / pdf뷰어

--- src/main/java/com/mycompany/webapp/controller/UserController.java
+++ src/main/java/com/mycompany/webapp/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mycompany.webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j2;
@@ -43,6 +44,17 @@ public class UserController {
    public String login() {
       log.info("실행");
       return "/user/login";
+   }
+   
+   @GetMapping("/contractForm")
+   public String contractForm() {
+	   return "/element/contractForm";
+   }
+   
+   @GetMapping("/contractFormPdf")
+   public String contractFormPdf() {
+	      log.info("실행");
+	   return "/element/contractFormPdf";
    }
    
 }
