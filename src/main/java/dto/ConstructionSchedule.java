00615b86f77ddbe44a941bb6bb91eb5f8ffb9997
kangyun9957 <55389539+kangyun9957@users.noreply.github.com>
2022-04-25 21:15:18 +0900

    cs rd dto추가

--- src/main/java/dto/ConstructionSchedule.java
+++ src/main/java/dto/ConstructionSchedule.java
@@ -1,5 +1,17 @@
 package dto;
 
-public class ConstructionSchedule {
+import java.util.Date;
 
+public class ConstructionSchedule {
+	
+	int ConsScheId;
+	String  ConsScheTeamId;
+	int  ConsScheContractId;
+	Date  ConsScheStartdate;
+	Date  ConsScheEnddate;
+	String  ConsScheContent;
+	String  ConsScheCategoryId;
+	String  ConsScheHospitalName;
+	String  ConsScheDln;
+	String  ConsScheAddress;
 }
--- src/main/java/dto/RequestDetails.java
+++ src/main/java/dto/RequestDetails.java
@@ -1,5 +1,21 @@
 package dto;
 
+import java.sql.Blob;
+import java.util.Date;
+
 public class RequestDetails {
+	
+	String RdDln;
+	String RdAddress;
+	int RdState;
+	Date RdApplicationdate;
+	String RdTitle;
+	int RdBudget;
+	int RdFloor;
+	String RdDesign;
+	Date RdCounDate;
+	String RdSpace;
+	Blob RdRefeImg;
+	Date RdRequestConsPeriod;
 
 }
