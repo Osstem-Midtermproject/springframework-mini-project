00cc7ee4909854d306a11f492ac6acb134373bcf
dlwndud30 <leejy3022@naver.com>
2022-04-25 19:32:00 +0900

    Users, AsSchdule, Progress DTO 생성

---
+++ src/main/java/dto/Progress.java
@@ -0,0 +1,17 @@
+package dto;
+
+import java.sql.Blob;
+import java.sql.Clob;
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Progress {
+	private Date pDate;
+	private String pDln;
+	private String pAddress;
+	private String pCategory;
+	private Blob pImage;
+	private Clob pContent;
+}
---
+++ src/main/java/dto/Users.java
@@ -0,0 +1,17 @@
+package dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Users {
+	private String userId;
+	private String uPassword;
+	private String uPhoneNumber;
+	private Date uMembershipDate;
+	private String uName;
+	private String uRole;
+	private String uDln;
+	private String uAddress;
+}
