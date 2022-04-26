14fc48f47d2aca0f44b7ae55575c0b7ed8588eee
he-hn <hyehyeon0329@gmail.com>
2022-04-26 20:11:26 +0900

    hospital processing 페이징 추가

--- src/main/java/com/mycompany/webapp/dao/test1.java
+++
@@ -1,5 +0,0 @@
-package com.mycompany.webapp.dao;
-
-public class test1 {
-	int as_s = 3;
-}
--- src/main/java/com/mycompany/webapp/dto/Team.java
+++ src/main/java/com/mycompany/webapp/dto/Team.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 //팀
 public class Team {
-	 String tId;		//팀식별번호
-	 int tSales;		//매출
-	 float tScore;		//점수
-	 String tCategory;	//분류(부서-AS/상담/전기/설비/도배/가구)
+	 String tid;		//팀식별번호
+	 int tsales;		//매출
+	 float tscore;		//점수
+	 String tcategory;	//분류(부서-AS/상담/전기/설비/도배/가구)
 }
